112297a5cad4a71d8c01c899fc3b6dd72da13bab
ezz <ezzelmallah.forsan@gmail.com>
2024-05-28 11:44:43 +0300

    added user input as a calculator

--- main.cpp
+++ main.cpp
@@ -2,20 +2,51 @@
 #include <cstdlib>
 #include "testApp.h"
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-int main() {
-    char name[] = "Ezz";
-   
-    printf("Welcome to %s calculator \n", name);
-    
+int main() 
+{
+    int x;
+    int y;
+    int checker = 0;
+    string name ;
+    char op;
     testApp test;
+    do {
+        checker = 0;
+        cout << "please enter your name: \n";
+        cin >> name;
+        printf("Hi  %s, welcome to the calculator program\n", &name);
+        cout <<"please insert the operator +,-,%,* \n";
+        cin >> op;
+        cout << "please enter the two numbers between them a space \n";
+        cin >> x >> y;
+        
+            switch(op)
+            {
+                case ('+'):
+                cout << "The answer is: " << test.add(x,y);
+                break;
+            
+                case ('-'):
+                cout << "The answer is: " << test.sub(x,y);
+                break;
+            
+                case ('*','x'):
+                cout << "The answer is: " << test.mult(x,y);
+                break;
+            
+                case ('%','/'):
+                cout <<"The answer is: " << test.devide(x,y);
+                break;
+
+                default:
+                cout << "please enter a valid Operator ";
+                checker = 1;
+            }            
+    } while(checker == 1);
+
     
-    cout << "The Added answer is = "<< test.add(4, 4) << endl;
-    
-    cout << "The Subtracted answer is = "<< test.sub(7,2) << endl;
-    
-    return 0;
 }
 
